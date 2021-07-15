5d49139670e786bf6ad0ae4829a458018a9b93d7
Cynthia2016 <cynthiads380@gmail.com>
2021-07-15 17:53:28 +0530

    Java

---
+++ PatternsPractice.java
@@ -0,0 +1,22 @@
+package com.homework;
+
+public class PatternsPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 for(int e=1;e<5;e++)
+		   {
+		   int alphabet=65;                          //value of A
+		   for(int j=1;j<=e;j++)
+		   {
+		   System.out.print((char)alphabet);
+		   alphabet++;
+		   }
+		   System.out.println();
+		   }	
+		 }
+
+}
+//A
+//ABC
+//ABCD
